77369d77a429c5691c3860ee960e2cdcdf305be6
Patrick Dan Lacuna <ptrkdan@gmail.com>
2019-03-18 11:02:43 +0900

    - Refined collider on player ship - Added rotation force to spawned meteor - Organized player prefab

---
+++ Assets/Scripts/Player Components/MeteorRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorRotator : MonoBehaviour
+{
+    [SerializeField] float rotateSpeedMin = 0f;
+    [SerializeField] float rotateSpeedMax = 90f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, Random.Range(rotateSpeedMin, rotateSpeedMax) * Time.deltaTime);
+    }
+}
