aca5f7e4f800dea59ec0f8f93aa806cecba8b3db
Patrick Dan Lacuna <ptrkdan@gmail.com>
2019-03-18 15:01:37 +0900

    - Added Contract Details overlay

--- Assets/Scripts/Loaders/MenuLoader.cs
+++ Assets/Scripts/Loaders/MenuLoader.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuLoader : MonoBehaviour {
     [SerializeField] Canvas worldCanvas;
     [SerializeField] Canvas contractSelectCanvas;
+    [SerializeField] Canvas contractDetailsCanvas;
+
+    private void Start() {
+        ResetHQScene();
+    }
+
+    private void ResetHQScene() {
+        worldCanvas.gameObject.SetActive(true);
+        contractSelectCanvas.gameObject.SetActive(false);
+        contractDetailsCanvas.gameObject.SetActive(false);
+    }
 
     public void OpenContractSelect() {
         contractSelectCanvas.gameObject.SetActive(true);
     }
 
-    public void StartContract() {
+    public void OpenContractDetails() {
+        contractSelectCanvas.gameObject.SetActive(false);
+        contractDetailsCanvas.gameObject.SetActive(true);
     }
 
     
