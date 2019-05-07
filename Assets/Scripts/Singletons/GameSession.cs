93dece1c1e04b257d796b45852174a1f0a1d15a4
Patrick Dan Lacuna <ptrkdan@gmail.com>
2019-05-07 13:38:02 +0900

    - Added Quit confirmation overlay to go back to the main menu.

--- Assets/Scripts/Singletons/GameSession.cs
+++ Assets/Scripts/Singletons/GameSession.cs
@@ -4,7 +4,18 @@ public class GameSession : MonoBehaviour
 {
     [SerializeField] ContractConfig activeContract;
     [SerializeField] bool isRunSuccessful;
+    [SerializeField] Canvas quitConfirmationOverlayPrefab;
 
     public ContractConfig ActiveContract { get => activeContract; set => activeContract = value; }
     public bool IsRunSuccessful { get => isRunSuccessful; set => isRunSuccessful = value; }
+
+    Canvas quitConfirmationOverlay;
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && quitConfirmationOverlay == null)
+        {
+            Time.timeScale = 0;
+            quitConfirmationOverlay = Instantiate(quitConfirmationOverlayPrefab);
+        }
+    }
 }
---
+++ Assets/Scripts/UI/QuitConfirmation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class QuitConfirmation : MonoBehaviour
+{
+    public void CancelQuit()
+    {
+        Time.timeScale = 1;
+        Destroy(gameObject);
+    }
+
+    public void ConfirmQuit()
+    {
+        Time.timeScale = 1;
+        Quit();
+    }
+
+    private static void Quit()
+    {
+        FindObjectOfType<SceneLoader>().GoToStartMenu();
+    }
+}
