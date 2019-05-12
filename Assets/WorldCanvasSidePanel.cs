a10deed6d7d86a7257332bb10eb46c9f9b32e7a1
Patrick Dan Lacuna <ptrkdan@gmail.com>
2019-05-12 20:13:44 +0900

    - Upgraded Unity to 2019.1.2f1 - Updated HQ scene layout - Updated UI Overlay

---
+++ Assets/WorldCanvasSidePanel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldCanvasSidePanel : MonoBehaviour
+{
+    [SerializeField] Image watchtowerSprite;
+    [SerializeField] Image hangarSprite;
+    [SerializeField] Image marketSprite;
+    [SerializeField] Image officeSprite;
+    [SerializeField] Image apartmentSprite;
+
+    [Header("Fade")]
+    [SerializeField] float fadeAlpha = 0.7f;
+    [SerializeField] float fadeDuration = 0.2f;
+
+    Image targetSprite;
+    bool isFadingOut = true;
+
+    private void Update()
+    {
+        if (targetSprite)
+        {
+            FadeSprite();
+        }
+    }
+
+    public void StartFadeWatchtower()
+    {
+        targetSprite = watchtowerSprite;        
+    }
+
+    public void StartFadeHangar()
+    {
+        targetSprite = hangarSprite;
+    }
+
+    public void StartFadeMarket()
+    {
+        targetSprite = marketSprite;
+    }
+
+    public void StartFadeOffice()
+    {
+        targetSprite = officeSprite;
+    }
+
+    public void StartFadeApartment()
+    {
+        targetSprite = apartmentSprite;
+    }
+
+    public void EndFade()
+    {
+        targetSprite.color = new Color(1, 1, 1, 1);
+        targetSprite = null;
+    }
+
+    private void FadeSprite()
+    {
+        if (isFadingOut)
+        {
+            targetSprite.CrossFadeAlpha(fadeAlpha, fadeDuration, true);
+            isFadingOut = false;
+        }
+        else
+        {
+            targetSprite.CrossFadeAlpha(1, fadeDuration, true);
+            isFadingOut = true;
+        }
+    }
+}
